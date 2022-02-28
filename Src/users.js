3664a544fda42340a49728de4bf305734f990523
Jamie Bunch <JamieBunch@ymail.com>
2022-02-28 12:08:34 -0500

    added firestore and postman

--- Src/users.js
+++ Src/users.js
@@ -1,24 +1,23 @@
-exports.createUser = (req, Res) => {
-  // first, let' do some validation.... (email, Password)
-  if (!req.body || !req.body.email || !req.body.password) {
-    // invalid request
-    res.status(400).send('invalid request')
-  }
+const { connectDb } = require('./dbConnect')
 
+exports.createUser = (req, res) => {
+  // first, let's do some validation... (email, password)
+  if(!req.body || !req.body.email || !req.body.password) {
+    // invalid request
+    res.status(400).send('Invalid request')
+    return
+  }
   const newUser = {
     email: req.body.email,
     password: req.body.password,
     isAdmin: false,
     userRole: 5,
-  };
-
-  const db = connectDb();
+  }
+  const db = connectDb()
   db.collection('users').add(newUser)
-  .then(doc => {
-      //TODO: create a JWT and send bak the token
-      res.send('Account Created')
-  })
-  .catch(err => res.status(500).send(err))
-
-
+    .then(doc => {
+      // TODO: create a JWT and send back the token
+      res.status(201).send('Account created')
+    })
+    .catch(err => res.status(500).send(err))
 }
